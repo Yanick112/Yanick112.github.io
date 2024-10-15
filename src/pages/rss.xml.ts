819da4052d27b3d7aa9946304d0d793a7d2fdde1
Yanick112 <lazysheng@163.com>
2024-10-15 22:52:26 +0800

    101506

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -47,10 +47,10 @@ export async function GET() {
       };
     }),
     customData: `
-<follow_challenge>
-    <feedId>69025261727522816</feedId>
-    <userId>47967201307251712</userId>
-</follow_challenge>
+    <follow_challenge>
+        <feedId>69025261727522816</feedId>
+        <userId>47967201307251712</userId>
+    </follow_challenge>
     `,
   };
 
